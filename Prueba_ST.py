2789b569b21d1e2b71200b62afbcc98a6e856433
cjd1011 <77751497+cjd1011@users.noreply.github.com>
2023-05-18 16:03:35 -0500

    Update Prueba_ST.py

--- Prueba_ST.py
+++ Prueba_ST.py
@@ -8,3 +8,7 @@ st.write("Hello world")
 st.title(':clipboard: Análisis de la Bolsa de Valores Australiana') #Titulo del Dash
 st.subheader('Realizado por: Camilo Diaz')
 st.markdown('##')
+
+df = pd.read_excel("Portafolio Australia.xlsx")
+
+st.dataframe(df)
