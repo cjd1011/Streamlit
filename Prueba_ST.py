2e585a2ee8a1e49037119ddf0a88957b02beb525
cjd1011 <77751497+cjd1011@users.noreply.github.com>
2023-05-18 16:33:49 -0500

    Add files via upload

--- Prueba_ST.py
+++ Prueba_ST.py
@@ -15,19 +15,18 @@ df = pd.read_excel("Portafolio Australia.xlsx")
 #st.dataframe(df)
 
 st.sidebar.header("Activos a filtrar:") #sidebar lo que nos va a hacer es crear en la parte izquierda un cuadro para agregar los filtros que queremos tener
-    Activo = st.sidebar.multiselect(
+Activo = st.sidebar.multiselect(
         "Seleccione el Activo:",
         options = df['Asset'].unique(),
         default = df['Asset'].unique() #Aqui podría por default dejar un filtro especifico pero vamos a dejarlos todos puestos por default
     )
     
-    df_seleccion = df.query("Asset == @Activo " ) #el primero es la columna y el segundo es el selector
+df_seleccion = df.query("Asset == @Activo " ) #el primero es la columna y el segundo es el selector
 
-    st.dataframe(df_seleccion)
+st.dataframe(df_seleccion)
     
-    line = alt.Chart(df_seleccion).mark_line().encode(
+line = alt.Chart(df_seleccion).mark_line().encode(
         alt.X("Date",title = "Date"),
         alt.Y("Close", title = "Closing Price",scale=alt.Scale(zero=False)),
-        color = 'Asset'
-    )
-    line
+        color = 'Asset')
+line
